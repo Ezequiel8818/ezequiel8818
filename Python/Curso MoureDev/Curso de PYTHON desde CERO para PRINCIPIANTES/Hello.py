821b5da8998676851377aa6d9eee2156f4e3274a
ezequiel8818 <ezequielgarcianeyra@gmail.com>
2024-09-10 21:14:40 -0600

    feat: update desktop.ini

---
+++ Python/Curso MoureDev/Curso de PYTHON desde CERO para PRINCIPIANTES/Hello.py
@@ -0,0 +1,22 @@
+#Esto es un comentario: 
+#Nuestro Hola Mundo
+print("Hola Python")
+print('Hola Python')
+
+
+"""
+*Este un comentario para resaltar algo
+!Este es un comentario de advertencia
+?Este es un comentario para dudas
+"""
+
+'''
+Comentario con una linea 
+'''
+
+#Consultar el tip de dato
+print(type("Hola Mundo")) #Tipo String
+print(type(5)) #Tipo int
+print(type(1.8)) #Tipo float (Decimales)
+print(type(1+3j)) #Tipo complejo
+print(type(True)) #Tipo bool (booleano)
