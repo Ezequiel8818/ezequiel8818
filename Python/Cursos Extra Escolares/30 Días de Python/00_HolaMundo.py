cdf80d7647b0bdde2d94a2f3ccf4d4a065c55cff
ezequiel8818 <ezequielgarcianeyra@gmail.com>
2024-09-10 22:56:44 -0600

    feat: update 00_HolaMundo.py

---
+++ Python/Cursos Extra Escolares/30 Días de Python/00_HolaMundo.py
@@ -0,0 +1,2 @@
+#Función print(), imprimir en la terminal lo que esté dentro de los paréntesis
+print("Hola Mundo")
---
+++ Python/Cursos Extra Escolares/Mouredev/Principiantes/01_Variables.py
@@ -0,0 +1,17 @@
+#Variables
+
+my_string_variable = "My String Variable"
+print(my_string_variable)
+
+my_int_variable = 8
+print(my_int_variable)
+
+my_int_to_str_variable = str(my_int_variable) #Convierte el entero en tipo de dato string, str()
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable)) #Te dice que tipo de dato es el que está almacenado, type()
+
+my_bool_variable = False
+print(my_bool_variable)
+
+#Concatenación de variables en un print 
+print(my_string_variable, my_int_to_str_variable, my_bool_variable)
